f47f55762b69d5e66826f52ac7c81f7f8b856130
sassok <kevin.sassoo@gmail.com>
2021-06-09 15:23:32 +0200

    add users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users,
   controllers: {
       sessions: 'users/sessions',
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
   }
 get '/member-data', to: 'members#show'
 resources :accommodations
+get'/users-list', to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
